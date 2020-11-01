fc3a61f1c0a0304d082c84a78078485ac79789d4
jquinzell <>
2020-11-01 13:11:32 -0600

    fix server

--- backend/resolvers/index.ts
+++ backend/resolvers/index.ts
@@ -3,8 +3,9 @@ import serverResolver from './server'
 import userResolver from './users'
 import dateResolver from './date'
 import messageResolvers from './message'
+import { IResolvers } from 'apollo-server'
 
-export const resolvers = merge.all([
+export const resolvers = merge.all<IResolvers>([
   serverResolver,
   userResolver,
   dateResolver,
