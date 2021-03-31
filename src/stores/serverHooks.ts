da4412daa435f0ac76a9f6fa1bed3524523d1d26
jquinzell <>
2021-03-30 19:57:14 -0500

    refactor hook

--- src/stores/serverHooks.ts
+++ src/stores/serverHooks.ts
@@ -1,22 +1,13 @@
 import { useRootStore } from '../RootStoreContext'
-import { CreateServer } from './interfaces'
 
 export function useServerStore() {
   const rootStore = useRootStore()
   return {
     joinedServers: rootStore.joinedServers,
     currentServer: rootStore.currentServer,
-    createServer(data: CreateServer) {
-      return rootStore.createServer(data)
-    },
-    selectServer(title: string) {
-      return rootStore.selectServer(title)
-    },
-    postMessage(text: string) {
-      return rootStore.postMessage(text)
-    },
-    login(username: string, password: string) {
-      return rootStore.login(username, password)
-    },
+    createServer: rootStore.createServer,
+    selectServer: rootStore.selectServer,
+    postMessage: rootStore.postMessage,
+    login: rootStore.login,
   }
 }
